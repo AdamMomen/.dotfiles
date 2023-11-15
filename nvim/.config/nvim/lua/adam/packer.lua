04ebbf15818cc21c80bbd28c134cd32fda5516bd
Adam Momen <adammuman81@gmail.com>
2023-11-15 14:58:25 +0800

    Add: chatgpt plugin

--- nvim/.config/nvim/lua/adam/packer.lua
+++ nvim/.config/nvim/lua/adam/packer.lua
@@ -2,16 +2,17 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { { 'nvim-lua/plenary.nvim' } } }
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { { 'nvim-lua/plenary.nvim' } } }
     use({ 'rose-pine/neovim', as = 'rose-pine', config = function() vim.cmd("colorscheme rose-pine") end })
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
     use('ThePrimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use('nvim-tree/nvim-web-devicons')
 
     -- use("thePrimeagen/git-worktree.nvim")
-
+    use("xiyaowong/telescope-emoji.nvim")
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
@@ -19,7 +20,7 @@ return require('packer').startup(function(use)
             -- LSP Support
             { 'neovim/nvim-lspconfig' }, -- Required
             {
-                              -- Optional
+                -- Optional
                 'williamboman/mason.nvim',
                 run = function()
                     pcall(vim.cmd, 'MasonUpdate')
@@ -27,9 +28,23 @@ return require('packer').startup(function(use)
             },
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },      -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },  -- Required
-            { 'L3MON4D3/LuaSnip' },      -- Required
+            { 'hrsh7th/nvim-cmp' },                  -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+            { 'L3MON4D3/LuaSnip' },                  -- Required
         }
     }
+    use({
+        "jackMort/ChatGPT.nvim",
+        config = function()
+            require("chatgpt").setup({
+                api_key_cmd = "cat ~/.chatgpt"
+            })
+        end,
+        requires = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
+    })
+    use("f-person/git-blame.nvim")
 end)
