a55e24a4146b746f907a756e7914a4cbc3040221
Adam Momen <adammuman81@gmail.com>
2023-11-15 15:26:24 +0800

    format treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,11 +1,15 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "javascript", "typescript", "rust", "c", "lua", "vim", "vimdoc", "query", "python"},
-
-  sync_install = false,
-
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    ensure_installed = { "javascript", "typescript", "rust", "c", "lua", "vim", "vimdoc", "query", "python" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true
+    },
+    folding = {
+        enable = true, -- Enable Treesitter folding
+    },
 }
