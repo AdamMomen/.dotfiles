63fa5bc2fca409a0e3db20f91e7e967540189f22
Adam Momen <adammuman81@gmail.com>
2023-11-15 15:27:32 +0800

    trying to get tmux work in harpoon

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -1,5 +1,6 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local tmux = require("harpoon.tmux")
 
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
@@ -9,3 +10,5 @@ vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<C-t>", function() tmux.goToTerminal(1, "{marked}")    end)
